9db6723d2eca0979419f7dd643d48444f32d5e16
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-06-28 13:37:34 -0500

    Remove dotenv config from index.js and dependencies.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// Load our environment variables.
-require('dotenv').config()
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
