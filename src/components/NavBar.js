c42d0f43eac03410e93c07fffbd696bd3f236c39
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-11 17:04:09 -0500

    Add componentDidMount login check to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,33 @@ import {ActionButton} from 'office-ui-fabric-react';
 export default class NavBar extends React.Component {
   constructor() {
     super();
-    this.state = { loggedIn: true };
+    this.state = { loggedIn: false };
 
     this.logout = this.logout.bind(this);
+    this.checkLogin = this.checkLogin.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkLogin();
+  }
+
+  async checkLogin() {
+    /* Checks if we're logged in and authenticated to our application. */
+    let url = process.env.REACT_APP_REDIRECT_URI + '/auth/validate';
+    let options = {
+      credentials: 'include',
+      method: 'GET',
+      mode: 'no-cors'
+    };
+    
+    try {
+      await fetch(url, options)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .then(res => this.setState({ loggedIn: res.loggedIn })); 
+    } catch (err) {
+      console.log(err, err.stack);
+    }
   }
 
   logout() {
